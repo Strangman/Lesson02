81df295fa9ca9c457eb84fa4762f6db5dd06ea62
Strangman <55458548+Strangman@users.noreply.github.com>
2019-09-18 00:45:38 +0300

    Add files via upload

---
+++ 1/Lessone02/Lessone02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessone02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beer = 6;
+            if (beer >= 60)
+            {
+                Console.WriteLine("Leffe");
+            }
+            else if (60 > beer & beer >= 45)
+            {
+                Console.WriteLine("Bud");
+            }
+            else if (45 > beer & beer >= 30)
+            {
+                Console.WriteLine("Stella Artois");
+            }
+            else if (30 > beer & beer >= 20)
+            {
+                Console.WriteLine("Chernigivske");
+            }
+            else if (20 > beer & beer >= 7)
+            {
+                Console.WriteLine("Obolon");
+            }
+            else
+            {
+                Console.WriteLine("Just water)");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2/Discount/Discount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cash = 532;
+            if (cash < 300)
+            {
+                Console.WriteLine("Your discount is 0 %, total to pay " + cash);
+            }
+            else if (cash >= 300 & 400 > cash)
+            {
+                Console.WriteLine("Your discount is 3 %, total to pay " + (cash-(cash*0.03)));
+            }
+            else if (cash >= 400 & 500 > cash)
+            {
+                Console.WriteLine("Your discount is 5 %, total to pay " + (cash - (cash * 0.05)));
+            }
+            else if (cash >= 500)
+            {
+                Console.WriteLine("Your discount is 7 %, total to pay " + (cash - (cash * 0.07)));
+            }
+            Console.ReadKey();
+        }
+    }
+}
