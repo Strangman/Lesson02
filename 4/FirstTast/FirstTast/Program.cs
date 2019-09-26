4840949f2ff066ef7ee8ef3e4fa26176ce764f1c
Strangman <kolos43@gmail.com>
2019-09-26 12:22:37 +0300

    Lesson04

---
+++ 4/FirstTast/FirstTast/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[11];
+            int[] rnd = { 1, 1, 1, 1, 1 };
+            Array.Copy(rnd,0 ,  arr, (arr.Length/2)-(rnd.Length/2), rnd.Length);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                
+                Console.Write(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4/SecondTask/SecondTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] month = new int[12];
+            int i = 0;
+            do
+            {
+                month[i] = 28 + (++i + (i / 8)) % 2 + 2 % i + 2 * (1 / i);
+            } while (i < month.Length);
+            i = 0;
+            do
+            {
+                Console.WriteLine("There is " + month[i] + " days in " + ++i + " month");
+            } while (i < month.Length);
+            Console.ReadKey();
+        }
+    }
+}
