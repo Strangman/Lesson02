0ebc0318d392b43bf3dcfdae6ace2028cf8e7669
Strangman <kolos43@gmail.com>
2019-10-07 23:56:04 +0300

    Lesson07v3

---
+++ 7/Task4/Task3/Task3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colore color = new Colore();
+            Console.WriteLine("Chose you fighter:\n1.Circle\n2.Triangle\n3.Square\n4.Exit");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Figure circle = new Circle();
+                    Console.WriteLine("enter name: ");
+                    circle.getName(Console.ReadLine());
+                    Console.WriteLine("enter radius: ");
+                    circle.setSide(Convert.ToInt32(Console.ReadLine()));
+                    circle.setPerimetr();
+                    circle.getColore(color.getName());
+                    circle.getInfo();
+                    break;
+                case "2":
+                    Figure trio = new Triangle();
+                    Console.WriteLine("enter name: ");
+                    trio.getName(Console.ReadLine());
+                    Console.WriteLine("enter side: ");
+                    trio.setSide(Convert.ToInt32(Console.ReadLine()));
+                    trio.setPerimetr();
+                    trio.getColore(color.getName());
+                    trio.getInfo();
+                    break;
+                case "3":
+                    Figure square = new Square();
+                    Console.WriteLine("enter name: ");
+                    square.getName(Console.ReadLine());
+                    Console.WriteLine("enter side: ");
+                    square.setSide(Convert.ToInt32(Console.ReadLine()));
+                    square.setPerimetr();
+                    square.getColore(color.getName());
+                    square.getInfo();
+                    break;
+                case "4":
+                    Console.WriteLine("Bye");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+    class Colore
+    {
+        private string[] colores = new[] { "Red", "Blue", "Green", "Grey", "Yellow", "Purple", "Black", "Brown", "Orange", "Pink" };
+        private string name;
+        Random rnd = new Random();
+        public string getName()
+        {
+            return name = colores[rnd.Next(0, 10)];
+        }
+    }
+    class Figure
+    {
+        public string name;
+        public string colore;
+        public int side1;
+        public double perimetr;
+        public void getName(string name)
+        {
+            this.name = name;
+        }
+        public void getColore(string colore)
+        {
+            this.colore = colore;
+        }
+        public virtual void setSide(int side1)
+        {
+            this.side1 = side1;
+        }
+        public virtual void setPerimetr()
+        {
+            perimetr = 3 * side1;
+        }
+        public virtual void getInfo()
+        {
+            Console.WriteLine("name: " + name + " colore: " + colore);
+        }
+    }
+    class Circle: Figure
+    {
+        public override void setPerimetr()
+        {
+            perimetr = 3.14 * Math.Pow(side1, 2);
+        }
+        public override void getInfo()
+        {
+            Console.WriteLine("name: " + name + " colore: " + colore + " Area: " + perimetr);
+        }
+    }
+    class Triangle: Figure
+    {
+        public override void getInfo()
+        {
+            Console.WriteLine("name: " + name + " colore: " + colore + " Perimetr: " + perimetr);
+        }
+    }
+    class Square: Figure
+    {
+        public override void setPerimetr()
+        {
+            perimetr = 4 * side1;
+        }
+        public override void getInfo()
+        {
+            Console.WriteLine("name: " + name + " colore: " + colore + " Perimetr: " + perimetr);
+        }
+    }
+}
